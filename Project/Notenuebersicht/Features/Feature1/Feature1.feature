eaed4f392efeae7097b706e85e88074189762f66
Max Arndt <max@eyrichshof.de>
2017-07-12 15:57:09 +0200

    added notenuebersicht

---
+++ Project/Notenuebersicht/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="b9d2ee26-3639-4461-a438-cd0fc5cf0fbc" description="SharePoint-Add-In-Feature" featureId="b9d2ee26-3639-4461-a438-cd0fc5cf0fbc" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Notenuebersicht Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="6b8f71c6-6472-4051-a4e3-928b102dd5fd" />
+    <projectItemReference itemId="a465b4fc-2f2e-40fe-8d9a-67a7af4a9fac" />
+    <projectItemReference itemId="0615a6ab-ec08-4934-a400-e0418cc42b4d" />
+    <projectItemReference itemId="d9d1fc64-7c20-4d08-b6cc-a4ea23042ccf" />
+  </projectItems>
+</feature>
---
+++ Project/Notenuebersicht/Scripts/schueler.js
@@ -0,0 +1,46 @@
+﻿{
+  "name": "Klara Klinger",
+  "jahrgaenge": [
+    {
+      "jahrgang": "2016/2017",
+      "klasse": "10B",
+      "halbjahreszeugnis": "",
+      "jahreszeugnis": "",
+      "notendurchschnitt": "3.1",
+      "faecher": [
+        {
+          "name": "Deutsch",
+          "notendurchschnitt": "3.1",
+          "noten": [
+            {
+              "datum": "13.06.2017",
+              "typ": "Schulaufgabe",
+              "note": 2
+            },
+            {
+              "datum": "01.06.2017",
+              "typ": "Ex",
+              "note": 4
+            }
+          ]
+        },
+        {
+          "name": "Mathematik",
+          "notendurchschnitt": "2.0",
+          "noten": [
+            {
+              "datum": "13.04.2017",
+              "typ": "Schulaufgabe",
+              "note": 3
+            },
+            {
+              "datum": "10.06.2017",
+              "typ": "Abfrage",
+              "note": 5
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
